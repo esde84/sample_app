4bb4bf5bdfbc9fee89ce24e8872cd15f047ed87d
Scott <esde84@gmail.com>
2011-05-30 16:39:07 +0100

    Added Password Hash Functionality

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -101,6 +101,19 @@ describe User do
       @user.encrypted_password.should_not be_blank
     end
     
+    
+    describe "has_password? method" do
+      
+      it "should be true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+      
+      it "should be false if the passwords do not match" do
+        @user.has_password?("invalid").should be_false
+      end
+      
+    end
+    
   end
   
 end
